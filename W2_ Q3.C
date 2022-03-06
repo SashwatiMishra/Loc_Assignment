3f9ef1829084dc131b7de8dfd5ab12ac99147d9d
SashwatiMishra <99500314+SashwatiMishra@users.noreply.github.com>
2022-03-06 12:01:23 +0530

    Create W2_ Q3.C

---
+++ W2_ Q3.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  int num, last, first, temp, swap, count = 0;
+  printf("Enter any number:\n");
+  scanf("%d", &num);
+  temp = num;
+  while(temp>=10)
+    {
+        last = temp % 10;
+        temp =  temp/10;
+    }
+  first = temp;
+  swap = (last * pow(10, count) + first) + (num - (first * pow(10, count) + last));
+  printf("Last Digit: %d\n", last);
+  printf("First Digit: %d\n", first);
+  printf("%d is swapped to %d\n", num, swap);
+  return 0;
+}
