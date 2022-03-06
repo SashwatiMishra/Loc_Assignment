1111249dd5ae6e8f84d15cc0ecfe82f77ab772e3
SashwatiMishra <99500314+SashwatiMishra@users.noreply.github.com>
2022-03-06 13:07:02 +0530

    Update W2_ Q1.C

--- W2_ Q1.C
+++ W2_ Q1.C
@@ -1,10 +1,10 @@
+//To concatenate strings and print it reverse order//
 #include<stdio.h>
 #include <string.h>
- 
 int main()
 {
     char s1[100],s2[100];  
-    int i,j;
+    int i,j,k,temp;
  
     printf("Enter  string1: ");
     scanf("%s",&s1);
@@ -19,5 +19,13 @@ int main()
     s1[i+j]='\0';
  
     printf("combined two strings ='%s'\n",s1);
+    k=i + j;
+    for (k = 0; k<len/2; k++)  
+    {   
+        temp = s1[k];  
+        s1[k] = s1[len - k - 1];  
+        s1[len - k - 1] = temp;  
+    } 
+    printf("reverse string= %s\n",s1);
     return 0;
 }
