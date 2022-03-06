301028b75b15c22a17628aa1b1aabfb57f3ef8b0
SashwatiMishra <99500314+SashwatiMishra@users.noreply.github.com>
2022-03-06 10:42:31 +0530

    Create W2_ Q6.C

---
+++ W2_ Q6.C
@@ -0,0 +1,21 @@
+include <stdio.h>
+int main()
+{
+   int i, j, N=5;
+   for(i=1; i<=N; i++) 
+   {
+      for(j=1; j<=N; j++)
+      {
+         if(i==1 || i==N || j==1 || j==N)
+         {
+            printf("*");
+         }
+         else
+         {
+            printf(" ");
+         }
+      }
+      printf("\n");
+   }
+   return 0;
+}
