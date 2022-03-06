95f6493630325517ca9a8aa999d9f2e0504336c2
SashwatiMishra <99500314+SashwatiMishra@users.noreply.github.com>
2022-03-06 15:37:03 +0530

    Update W2_ Q3.C

--- W2_ Q3.C
+++ W2_ Q3.C
@@ -3,17 +3,18 @@
 #include <math.h>
 int main()
 {
-  int num, last, first, temp, swap, count = 0;
+  int num, last, first, temp, swap, digit = 0;
   printf("Enter any number:\n");
   scanf("%d", &num);
   temp = num;
   while(temp>=10)
     {
         last = temp % 10;
+        digit++;
         temp =  temp/10;
-    }
+     }
   first = temp;
-  swap = (last * pow(10, count) + first) + (num - (first * pow(10, count) + last));
+  swap = (last * pow(10, digit) + first) + (num - (first * pow(10, digit) + last));
   printf("Last Digit: %d\n", last);
   printf("First Digit: %d\n", first);
   printf("%d is swapped to %d\n", num, swap);
