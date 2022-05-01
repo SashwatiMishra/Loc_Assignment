6210194c769040e21d1c466b44523fefbed9bd8c
SashwatiMishra <99500314+SashwatiMishra@users.noreply.github.com>
2022-05-01 11:40:28 +0530

    Create W3_Q3.C

---
+++ W3_Q3.C
@@ -0,0 +1,28 @@
+/*A C program to check whether a number is even or odd
+using functions
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+int find_Num(int);
+int main()
+{
+    int num;
+    clrscr();
+    printf("Enter a number to check odd or even\n");
+    scanf("%d",&num);
+    find_Num(num);
+    return 0;
+}
+int find_Num(int num)
+{
+ if(num%2==0)
+ {
+    printf("\n%d is an even number",num);
+ }
+ else
+ {
+    printf("\n%d is an odd number",num);
+ }
+
+}
